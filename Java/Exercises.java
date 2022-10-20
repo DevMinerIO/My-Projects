3e6519d9f3626992581c2fda70c19e4512d8d490
MINERio <mnheat.miner@gmail.com>
2022-10-20 13:54:33 -0500

    first java exercises commit

---
+++ Java/Exercises.java
@@ -0,0 +1,11 @@
+/**
+ * HERE ARE MY EXAMPLE EXERCISES AND PROBLEMS I HAVE SOLVED
+ */
+
+public class Exercises {
+   public static void main(String[] args) {
+      // simple console logs to ensure my new vs code setup is working for Java
+      System.out.println("Hello i am working");
+      System.out.println("test 2");
+   }
+}
