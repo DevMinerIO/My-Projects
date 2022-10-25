03590bf54dabf9696be7e3792b0dd01bcacaf8fa
MINERio <mnheat.miner@gmail.com>
2022-10-25 15:10:19 -0500

    days of the month challenge completed

--- Java/Exercises.java
+++ Java/Exercises.java
@@ -224,4 +224,70 @@ public class Exercises {
       return firstNumber + lastNumber;
    }
 
+   // Calculate the number of days in a month, return true if it is a leap year.
+   public static boolean isLeapYear(int year) {
+      if (year < 1 || year > 9999) {
+         return false;
+      }
+      if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+   public static int getDaysInMonth(int month, int year) {
+      int days = -1;
+      if (month < 1 || month > 12) {
+         return -1;
+      }
+      if (year < 1 || year > 9999) {
+         return -1;
+      }
+
+      switch (month) {
+         case 1:
+            days = 31;
+            break;
+         case 2:
+            if (isLeapYear(year) == false) {
+               days = 28;
+            } else {
+               days = 29;
+            }
+            break;
+         case 3:
+            days = 31;
+            break;
+         case 4:
+            days = 30;
+            break;
+         case 5:
+            days = 31;
+            break;
+         case 6:
+            days = 30;
+            break;
+         case 7:
+            days = 31;
+            break;
+         case 8:
+            days = 31;
+            break;
+         case 9:
+            days = 30;
+            break;
+         case 10:
+            days = 31;
+            break;
+         case 11:
+            days = 30;
+            break;
+         case 12:
+            days = 31;
+            break;
+      }
+      return days;
+   }
+
 }
