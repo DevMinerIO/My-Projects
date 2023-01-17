42ced86fc1044d39522290fc6b7974b61e55734a
MINERio <poeminminer1@gmail.com>
2023-01-17 17:27:22 -0600

    added basic nav bar. Still have to finish routes

--- team-randomizer/src/App.js
+++ team-randomizer/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom"
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        {/* TODO future routes */}
+        {/* <Routes> */}
+        {/* <Route path="/" element={<Home />} />
+          <Route path="/switchOne" element={<Switch />} />
+          <Route path="/draft" element={<Draft />} /> */}
+        {/* </Routes> */}
+      </div>
+    </>
   );
 }
 
